378a0c889a29773594432a2cb50163cdce08eccd
Rosen Yorgov <iorgovaa@gmail.com>
2021-11-20 17:41:23 +0200

    fetch all pets from db

--- my-pet/src/components/pet/AllPets.js
+++ my-pet/src/components/pet/AllPets.js
@@ -1,13 +1,44 @@
+import { useState, useEffect } from 'react';
+
+import * as petService from '../../services/petService';
+
 import './partials/PetPartial';
 import PetPartial from './partials/PetPartial';
 
 function AllPets(){
-   return(
-      <div className="allPetsContainer">
 
-         <PetPartial />            
-         <PetPartial />            
-         <PetPartial />            
+   const [pets, setPets] = useState([]);
+
+   useEffect(() => {
+      async function fetch(){
+         let result = await petService.getAll();
+
+         setPets(result);
+      }
+
+      fetch();
+   }, []);
+
+   return(
+      <div >
+         <h3>ALL PETS</h3>
+
+         <div className="allPetsContainer">
+            
+            {
+               pets.length > 0 
+               ? pets.map(x => 
+                  
+                  <PetPartial 
+                     key={x._id}
+                     pet={x}
+                  />
+                  )
+                  : <p>No pets found yet.</p>
+            }
+
+         </div>
+                  
          
       </div>
    );
