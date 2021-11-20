49e5f27c5bd2c8fa2e47628b97d105841c960feb
Rosen Yorgov <iorgovaa@gmail.com>
2021-11-20 18:26:57 +0200

    extracted the active styles into an object

--- my-pet/src/components/header/Header.js
+++ my-pet/src/components/header/Header.js
@@ -1,20 +1,20 @@
 import {Link, NavLink} from 'react-router-dom'
 
 function Header(){
-
+    let activeStyles = {backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'};
 
    return (
       <header>
         <div className="navigation">
             <nav>
                 <ul>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/">HOME</NavLink></li>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}}to="">MY PETS</NavLink></li>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/pets/all">ALL PETS</NavLink></li>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/user/myprofile">MY PROFILE</NavLink></li>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/user/register">REGISTER</NavLink></li>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/user/login">LOGIN</NavLink></li>
-                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="">LOGOUT</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles} to="/">HOME</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles}to="">MY PETS</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles} to="/pets/all">ALL PETS</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles} to="/user/myprofile">MY PROFILE</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles} to="/user/register">REGISTER</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles} to="/user/login">LOGIN</NavLink></li>
+                    <li><NavLink activeStyle={activeStyles} to="">LOGOUT</NavLink></li>
                 </ul>
             </nav>
         </div>
