5a2ef8f0072c5083c5203d5b982b88131e0ca1af
Rosen Yorgov <iorgovaa@gmail.com>
2021-11-20 17:41:34 +0200

    fetch a single pet from db

--- my-pet/src/components/pet/Details.js
+++ my-pet/src/components/pet/Details.js
@@ -1,34 +1,49 @@
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import * as petService from '../../services/petService';
 
+const Details = ({
+    match,
+}) => {
+    const [pet, setPet] = useState({});
 
-function Details(){
+    useEffect(() => {
+
+        petService.getOne(match.params.petId)
+            .then(result => {
+                setPet(result)
+
+            });
+        
+    }, []);
+
    return (
       <div className="details">
+          
+                <div className="main">
+                    <div className="left">
+                        <div className="petDetails">
+                            <h1>Pet Name : {pet.petName} </h1>
+                            <h3>Owner : </h3>
+                            <div className="card">
+                                <p>Breed : {pet.breed} </p>
+                                <p>Age : {pet.age} </p>
+                                <p>Type : {pet.type} </p>
+                            </div>
 
-          <div class="main">
-              <div class="left">
-                  <div class="petDetails">
 
-                      <h1>Pet Name : </h1>
-                      <h3>Owner : </h3>
-                      <div className="card">
-                          <p>Breed : </p>
-                          <p>Age : </p>
-                          <p>Type : </p>
-                      </div>
+                            <div className="buttonsContainer">
+                                <Link to={`/pets/edit/${pet._id}`} className="edit-btn">Edit</Link>
+                                <Link to={`/pets/delete/${pet._id}`} className="del-btn">Delete</Link>
+                            </div>
 
-
-                      <div className="buttonsContainer">
-                          <a href="" class="edit-btn">Edit</a>
-                          <a href="" class="del-btn">Delete</a>
-                      </div>
-
-                  </div>
-              </div>
-              <div className="right">
-                  <img src="https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Dog-512.png" alt="image"
-                      width="200px" height="200px" />
-              </div>
-          </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <img src={pet.imageURL} alt="image"
+                            width="200px" height="200px" />
+                    </div>
+                </div>
 
       </div>
    );
