c6cbd5b0914bb09cc0e9cb12c3b3eaab87f80875
Rosen Yorgov <iorgovaa@gmail.com>
2021-11-20 17:41:55 +0200

    pet partial for the all pets page

--- my-pet/src/components/pet/partials/PetPartial.js
+++ my-pet/src/components/pet/partials/PetPartial.js
@@ -1,12 +1,20 @@
-function PetPartial(){
+import {Link} from 'react-router-dom'
+
+const PetPartial = ({
+   pet,
+}) => {
    return(
       <div className="petPartial">
-               <img src="https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Dog-512.png" alt="petPicture"
+               <img src={pet.imageURL} alt="petPicture"
                   width="100px" height="100px" />
-               <h3> pet name</h3>
-               <p> pet age</p>
-               <p> breed</p>
-               <a href=""> ped details</a>
+               <h3> {pet.petName}</h3>
+               <p> {pet.age}</p>
+               <p> {pet.breed}</p>
+               
+               <Link 
+               to={`/pets/details/${pet._id}`} 
+               className="detailsBtn"> Details
+               </Link>
       </div>
    )
 }
