aea8680e001f006fc7062b243afb59ed7566ba0e
Rosen Yorgov <iorgovaa@gmail.com>
2021-11-20 18:24:45 +0200

    added activeStyles to the navbar buttons

--- my-pet/src/components/header/Header.js
+++ my-pet/src/components/header/Header.js
@@ -1,18 +1,20 @@
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 
 function Header(){
+
+
    return (
       <header>
         <div className="navigation">
             <nav>
                 <ul>
-                    <li><Link to="/">HOME</Link></li>
-                    <li><Link to="">MY PETS</Link></li>
-                    <li><Link to="/pets/all">ALL PETS</Link></li>
-                    <li><Link to="/user/myprofile">MY PROFILE</Link></li>
-                    <li><Link to="/user/register">REGISTER</Link></li>
-                    <li><Link to="/user/login">LOGIN</Link></li>
-                    <li><Link to="">LOGOUT</Link></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/">HOME</NavLink></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}}to="">MY PETS</NavLink></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/pets/all">ALL PETS</NavLink></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/user/myprofile">MY PROFILE</NavLink></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/user/register">REGISTER</NavLink></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="/user/login">LOGIN</NavLink></li>
+                    <li><NavLink activeStyle={{backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'}} to="">LOGOUT</NavLink></li>
                 </ul>
             </nav>
         </div>
