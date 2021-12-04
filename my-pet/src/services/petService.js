5e84c4b6511decd88bfaa77b71cc1afe0fe462ef
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-05 00:32:54 +0200

    refactored front end petService create method

--- my-pet/src/services/petService.js
+++ my-pet/src/services/petService.js
@@ -7,21 +7,22 @@ export function getAll() {
 
 export const getOne = (id) => fetch(`${baseURL}/pets/${id}`).then(res => res.json());
 
-export const createPet = (petName, breed, age, type, imageURL) => 
-    fetch(`${baseURL}/pets/create`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                petName,
-                breed,
-                age,
-                type,
-                imageURL
-            })
-        })
-        .then(res => {return res.json()});
+export const createPet = (petName, breed, age, type, imageURL, userId) => 
+            fetch(`${baseURL}/pets/create`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            petName,
+                            breed,
+                            age,
+                            type,
+                            imageURL,
+                            userId
+                        })
+                    })
+                    .then(res => {return res.json()});
 
 
 export const getTopThreeAdded = () => fetch(`${baseURL}`)
