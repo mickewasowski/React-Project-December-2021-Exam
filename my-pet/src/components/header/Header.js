7d0b738bea77c30157c2f4b565948f0747c734fa
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-05 00:31:04 +0200

    refactored Header to be dynamic for logged or guest users

--- my-pet/src/components/header/Header.js
+++ my-pet/src/components/header/Header.js
@@ -1,28 +1,46 @@
 import styles from './Header.module.css';
+import { useAuth } from '../../contexts/UserContext';
 
-import {Link, NavLink} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 
 function Header(){
+    const { user } = useAuth();
+
     let activeStyles = {backgroundColor: 'white', color: 'black', fontWeight: 'bold', padding: '3px'};
 
-   return (
-      <header>
-        <div className={styles.navigation}>
-            <nav>
-                <ul>
-                    <li><NavLink activeStyle={activeStyles} to="/">HOME</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles}to="/pets/myPets">MY PETS</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles} to="/pets/all">ALL PETS</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles} to="/pets/create">CREATE</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles} to="/user/myprofile">MY PROFILE</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles} to="/user/register">REGISTER</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles} to="/user/login">LOGIN</NavLink></li>
-                    <li><NavLink activeStyle={activeStyles} to="">LOGOUT</NavLink></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-   );
+    return (
+        <header>
+                {
+                    user.userId !== ''
+                    ? <div className={styles.nameDiv}><h3>Hello, {user.fullName}!</h3></div>
+                    : ''
+                }
+            <div className={styles.navigation}>
+                <nav>
+                    <ul>
+                        <li><NavLink activeStyle={activeStyles} to="/">HOME</NavLink></li>
+                       
+                       {
+                           user.userId !== ''
+                           ? <>
+                                <li><NavLink activeStyle={activeStyles}to="/pets/myPets">MY PETS</NavLink></li>
+                                <li><NavLink activeStyle={activeStyles} to="/pets/all">ALL PETS</NavLink></li>
+                                <li><NavLink activeStyle={activeStyles} to="/pets/create">CREATE</NavLink></li>
+                                <li><NavLink activeStyle={activeStyles} to="/user/myprofile">MY PROFILE</NavLink></li>
+                                <li><NavLink activeStyle={activeStyles} to="">LOGOUT</NavLink></li>
+                            </>
+                           :
+                            <>
+                                <li><NavLink activeStyle={activeStyles} to="/user/register">REGISTER</NavLink></li>
+                                <li><NavLink activeStyle={activeStyles} to="/user/login">LOGIN</NavLink></li>
+                            </>
+                       }
+ 
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
