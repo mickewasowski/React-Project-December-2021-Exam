dc3dba1e79cff5b5d098c62eac9255696269fd0f
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-05 00:24:00 +0200

    refactored login component to save user info in the UserContext

--- my-pet/src/components/user/Login.js
+++ my-pet/src/components/user/Login.js
@@ -1,10 +1,43 @@
+import {useState, useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+import {UserContext} from '../../contexts/UserContext';
+
 import styles from './Login.module.css';
 
+import * as userService from '../../services/userService';
+
 function Login(){
-   return( <form>
+   const [error, setError] = useState('');
+
+   let history = useHistory();
+   const { login } = useContext(UserContext);
+
+   const submitHandler = async (e) => {
+      
+      e.preventDefault();
+
+      const formData = new FormData(e.currentTarget);
+      let username = formData.get('username');
+      let password = formData.get('password');
+
+      userService.login(username, password)
+         .then(res => {
+            if (res.userId) {
+               setError('');
+               login(res.userId, res.username, res.email, res.fullName);
+               history.push('/');
+            }else{
+               setError(res.message);
+            }
+         });
+   }
+
+   return( 
+   <form onSubmit={submitHandler} className={styles.loginForm}>
       <div className="login">
             <div className={styles.formHeadings}>
                <h3>LOGIN</h3>
+               <p className={styles.error}>{error}</p>
             </div>
             <div>
                <label>Username : </label>
