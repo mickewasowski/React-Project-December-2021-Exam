935cce237e41025e56724f50ed788653b01de027
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-05 01:55:45 +0200

    implemented My profile component functionality

--- my-pet/src/components/user/MyProfile.js
+++ my-pet/src/components/user/MyProfile.js
@@ -1,12 +1,31 @@
+import {useState, useEffect} from 'react';
+import { useAuth } from '../../contexts/UserContext';
+
+import * as userService from '../../services/userService';
+
 import styles from './MyProfile.module.css';
 
 function MyProfile(){
+   const {user} = useAuth();
+
+   const [userInfo, setUserInfo] = useState({});
+   const [pets, setPets] = useState({});
+
+   useEffect(() => {
+      userService.getById(user.userId)
+         .then(res => {
+            setUserInfo(res);
+            setPets(res.myPets);
+         });
+   }, []);
+
    return (
       <div className={styles.myProfile}>
          <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png"
             alt="avatar" width="200px" height="200px" />
-         <h3> username </h3>
-         <p> pets owned </p>
+         <h3>Username: {userInfo.username}</h3>
+         <h4>Full name: {userInfo.fullName}</h4>
+         <p>Pets owned: {pets.length}</p>
       </div>
    )
 }
