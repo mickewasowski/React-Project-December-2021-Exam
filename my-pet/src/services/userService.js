fb3aad9ed393ec294b3bc09dcc80995801e3650d
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-05 00:34:55 +0200

    implemented fetch register and login front end

---
+++ my-pet/src/services/userService.js
@@ -0,0 +1,32 @@
+const baseURL = 'http://localhost:5000';
+
+export function login(username, password) {
+   return fetch(`${baseURL}/user/login`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+         username,
+         password
+      })
+   })
+   .then(res => {return res.json()});
+}
+
+export function register(username, fullName, email, password, rePassword){
+   return fetch(`${baseURL}/user/register`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+         username, 
+         fullName, 
+         email, 
+         password, 
+         rePassword
+      })
+   })
+   .then(res => {return res.json()});
+}
