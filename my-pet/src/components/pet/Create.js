f168769d1cd7f192ffe2d640a97ceb2b2d21270c
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-12 22:45:45 +0200

    refactored the code and implemented file upload for image

--- my-pet/src/components/pet/Create.js
+++ my-pet/src/components/pet/Create.js
@@ -3,6 +3,7 @@ import styles from './Create.module.css';
 import {useAuth} from '../../contexts/UserContext';
 import {isAuth} from '../../hoc/isAuth';
 
+import {uploadImage} from '../../services/cloudinaryService';
 import * as petService from '../../services/petService';
 import { useHistory } from 'react-router-dom';
 
@@ -20,13 +21,28 @@ function Create() {
         let breed = formData.get('breed');
         let age = formData.get('age');
         let type = formData.get('type');
-        let imageURL = formData.get('imageURL');
-        
-        let response = await petService.createPet(petName, breed, age, type, imageURL, user.userId);
+        let imageFile = formData.get('imageFile');
 
-        if(response._id){
-            //history.push('/pets/all');
-            history.push(`/pets/details/${response._id}`);
+        const cloudForm = new FormData();
+        cloudForm.append('file', imageFile);
+        cloudForm.append('upload_preset', 'g6y3jaem');
+        cloudForm.append('cloud_name', 'dr2keg2us');
+
+        try {
+            let publicImageId = await uploadImage(cloudForm);
+
+            console.log(publicImageId);
+
+            petService
+            .createPet(petName, breed, age, type, publicImageId, user.userId)
+            .then(res => {
+                console.log(res);
+                if (res._id) {
+                    history.push(`/pets/details/${res._id}`);
+                }
+            });
+        } catch (error) {
+            history.push('/failed');
         }
     }
 
@@ -40,25 +56,27 @@ function Create() {
                 <div>
                 <label>Pet name : </label>
                 <input type="text" placeholder="Enter Pet Name" name="petName" required />
-            </div><div>
-                <label>Breed : </label>
-                <input type="text" placeholder="Enter Breed" name="breed" required />
-            </div>
-            <div>
-                <label>Age : </label>
-                <input type="number" placeholder="Enter Age" name="age" required />
-            </div>
-            <div>
-                <label>Type : </label>
-                <input type="text" placeholder="Enter Type" name="type" required />
-            </div>
-            <div>
-                <label>Image URL :</label>
-                <input type="text" placeholder="Enter Image URL" name="imageURL" required />
-            </div>
-            <div className={styles.btnContainer}>
-                <button type="submit">ADD PET</button>
-            </div>
+                </div>
+                <div>
+                    <label>Breed : </label>
+                    <input type="text" placeholder="Enter Breed" name="breed" required />
+                </div>
+                <div>
+                    <label>Age : </label>
+                    <input type="number" placeholder="Enter Age" name="age" required />
+                </div>
+                <div>
+                    <label>Type : </label>
+                    <input type="text" placeholder="Enter Type" name="type" required />
+                </div>
+                <div>
+                    <label>Image file upload:</label>
+                    <input type="file" name="imageFile"/>
+                </div>
+
+                <div className={styles.btnContainer}>
+                    <button type="submit">ADD PET</button>
+                </div>
             </div>
         </form>
    );
