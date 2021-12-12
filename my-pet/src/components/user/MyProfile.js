96f50e280aec7ffe47fcabe88a8238dd6902a343
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-12 22:47:40 +0200

    added a catch to redirect to login in case of error

--- my-pet/src/components/user/MyProfile.js
+++ my-pet/src/components/user/MyProfile.js
@@ -1,4 +1,6 @@
 import {useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../contexts/UserContext';
 import {isAuth} from '../../hoc/isAuth';
 
@@ -8,6 +10,7 @@ import styles from './MyProfile.module.css';
 
 function MyProfile(){
    const {user} = useAuth();
+   const history = useHistory();
 
    const [userInfo, setUserInfo] = useState({});
    const [pets, setPets] = useState({});
@@ -17,16 +20,24 @@ function MyProfile(){
          .then(res => {
             setUserInfo(res);
             setPets(res.myPets);
+         })
+         .catch(() => {
+            history.push('/user/login');
          });
    }, [user]);
 
    return (
       <div className={styles.myProfile}>
-         <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png"
-            alt="avatar" width="200px" height="200px" />
-         <h3>Username: {userInfo.username}</h3>
-         <h4>Full name: {userInfo.fullName}</h4>
-         <p>Pets owned: {pets.length}</p>
+         <div>
+            <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png"
+               alt="avatar" width="200px" height="200px" />
+            <h3>Username: {userInfo.username}</h3>
+            <h4>Full name: {userInfo.fullName}</h4>
+            <p>Pets owned: {pets.length}</p>
+         </div>
+         <div className={styles.btnsContainer}>
+            <NavLink to="/user/changePassword">Update Password</NavLink>
+         </div>
       </div>
    )
 }
