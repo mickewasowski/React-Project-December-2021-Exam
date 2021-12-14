464b1219244f02fde14f0ccf001d3a57acfe864c
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-14 03:35:08 +0200

    implemented edit onSubmit functionality

--- my-pet/src/components/pet/Edit.js
+++ my-pet/src/components/pet/Edit.js
@@ -3,6 +3,7 @@ import styles from './Edit.module.css';
 import {useState, useEffect} from 'react';
 import {Image} from 'cloudinary-react';
 import { isAuth} from '../../hoc/isAuth';
+import { useHistory } from 'react-router-dom';
 
 import * as petService from '../../services/petService';
 
@@ -12,6 +13,7 @@ const Edit = ({
     match,
 }) => {
     const [pet, setPet] = useState({});
+    const history = useHistory();
 
     useEffect( () => {
         petService.getOne(match.params.petId)
@@ -23,6 +25,20 @@ const Edit = ({
 
     const submitHandler = async (e) => {
         e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        let petName = formData.get('petName');
+        let breed = formData.get('breed');
+        let age = formData.get('age');
+        let type = formData.get('type');
+
+        try {
+            await petService.editPet(pet._id, petName, breed, age, type);
+
+            history.push(`/pets/details/${pet._id}`);
+        } catch (error) {
+            history.push(`/error`);
+        }
     }
 
 
@@ -36,19 +52,19 @@ const Edit = ({
                 </div>
                 <div>
                     <label>Pet name : </label>
-                    <input type="text" defaultValue={pet.petName}></input>
+                    <input type="text" name="petName" defaultValue={pet.petName}></input>
                 </div>
                 <div>
                     <label>Breed : </label>
-                    <input type="text" defaultValue={pet.breed}></input>
+                    <input type="text" name="breed" defaultValue={pet.breed}></input>
                 </div>
                 <div>
                     <label>Age : </label>
-                    <input type="number" defaultValue={pet.age}></input>
+                    <input type="number" name="age" defaultValue={pet.age}></input>
                 </div>
                 <div>
                     <label>Type : </label>
-                    <input type="text" defaultValue={pet.type}></input>
+                    <input type="text" name="type" defaultValue={pet.type}></input>
                 </div>
                 <div className={styles.btnContainer}>
                     <button type="submit"> EDIT </button>
