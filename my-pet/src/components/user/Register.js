8bd3ef44a6a579e8851f618be5dbb4245581f78a
Rosen Yorgov <iorgovaa@gmail.com>
2021-12-17 23:00:59 +0200

    added errors partial

--- my-pet/src/components/user/Register.js
+++ my-pet/src/components/user/Register.js
@@ -6,6 +6,8 @@ import { isNotAuth } from '../../hoc/isAuth';
 
 import * as userService from '../../services/userService';
 
+import PopUpPartial from '../common/partials/PopUpNotification';
+
 function Register(){
    const [error, setError] = useState('');
    let history = useHistory();
@@ -21,46 +23,57 @@ function Register(){
       let rePassword = formData.get('rePassword');
 
       userService.register(username, fullName, email, password, rePassword)
-      .then(res => {
-         if (!res._id) {
-            return setError(res.message);
-         }
-         history.push('/user/login');
-      })
+         .then(res => {
+            if (res._id) {
+               history.push('/user/login');
+            }
+            else if (res.message.includes("User validation failed")){
+               let error = res.message.replace("User validation failed:", '');
+               return setError(error);
+            }
+         })
+         .catch(err => {
+            if (err.message.includes('Failed to fetch')) {
+               setError(err.message);
+            }
+            console.log(err)
+         });
    }
 
    return(
-      <form className={styles.registerForm} onSubmit={submitHandler}>
-            <div className="register">
-               <div className={styles.formHeadings}>
-                  <h3>REGISTER</h3>
-                  <p className={styles.error}>{error}</p>
-               </div>
-               <div>
-               <label>Username : </label>
-               <input type="text" placeholder="Enter Username" name="username" required />
-            </div>
-            <div>
-               <label>Full Name : </label>
-               <input type="text" placeholder="Enter Full Name" name="fullName" required />
-            </div>
-            <div>
-               <label>Email : </label>
-               <input type="text" placeholder="Enter Email" name="email" required />
-            </div>
-            <div>
-               <label>Password : </label>
-               <input type="password" placeholder="Enter Password" name="password" required />
-            </div>
-            <div>
-               <label>Repeat Password : </label>
-               <input type="password" placeholder="Enter Repeat Password" name="rePassword" required />
-            </div>
-            <div className={styles.btnContainer}>
-               <button type="submit">REGISTER</button>
-            </div>
-            </div>
-      </form>
+      <>
+            <PopUpPartial error={error} />
+            <form className={styles.registerForm} onSubmit={submitHandler}>
+                  <div className="register">
+                     <div className={styles.formHeadings}>
+                        <h3>REGISTER</h3>
+                     </div>
+                     <div>
+                     <label>Username : </label>
+                     <input type="text" placeholder="Enter Username" name="username" required />
+                  </div>
+                  <div>
+                     <label>Full Name : </label>
+                     <input type="text" placeholder="Enter Full Name" name="fullName" required />
+                  </div>
+                  <div>
+                     <label>Email : </label>
+                     <input type="text" placeholder="Enter Email" name="email" required />
+                  </div>
+                  <div>
+                     <label>Password : </label>
+                     <input type="password" placeholder="Enter Password" name="password" required />
+                  </div>
+                  <div>
+                     <label>Repeat Password : </label>
+                     <input type="password" placeholder="Enter Repeat Password" name="rePassword" required />
+                  </div>
+                  <div className={styles.btnContainer}>
+                     <button id={styles.registerBtn} type="submit">REGISTER</button>
+                  </div>
+                  </div>
+            </form>
+      </>
    )
 }
 
